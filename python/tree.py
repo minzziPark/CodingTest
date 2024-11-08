a38a7c8580454986521d3fa0bbe64cdef84fce31
minzziPark <minzzi0068@gmail.com>
2024-11-08 23:25:37 +0900

    add : 트리 기초 추가

---
+++ python/tree.py
@@ -0,0 +1,44 @@
+# 시간복잡도 : O(n)
+from collections import deque
+
+def levelOrder(root):
+	visited = []
+	if root is None:
+		return 0
+	q = deque()
+	q.append(root)
+	while q:
+		cur_node = q.popleft()
+		visited.append(cur_node.value)
+
+		if cur_node.left:
+			q.append(cur_node.left)
+		if cur_node.right:
+			q.append(cur_node.right)
+	return visited
+
+def traversal(root):
+	if root is None:
+		return
+	traversal(root.left)
+	traversal(root.right)
+
+def preorder(root):
+	if root is None:
+		return
+	print(root)
+	preorder(root.left)
+	preorder(root.right)
+
+def inorder(root):
+	if root is None:
+		inorder(root.left)
+		print(root)
+		inorder(root.right)
+
+def postorder(root):
+	if root is None:
+		return
+	postorder(root.left)
+	postorder(root.right)
+	print(root)
